e34e398eb9cf29f89c27ce1e29e148c172b79e7a
Rachel Hamby <tnthreat33@gmail.com>
2023-11-02 23:25:38 -0400

    done

--- lib/regex.py
+++ lib/regex.py
@@ -3,10 +3,10 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"[A-Za-z0-9_]"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"[0-9]"
 phone_regex = re.compile(phone_number)
 
 email_address = r""
